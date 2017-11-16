c5defdd2799c78d136a5e8ab562e7a5ebf5319b7
Michelle <30472336+michkac@users.noreply.github.com>
2017-11-16 12:46:48 -0500

    Add files via upload

---
+++ dev/scripts/form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+
+const Form = (props) => {
+	
+	const inputPropsStart = {
+		value: props.startAdd,
+		onChange: props.handleChangeStart,
+	}
+	const inputPropsEnd = {
+		value: props.endAdd,
+		onChange: props.handleChangeEnd,
+	}
+
+	litCommute.getCommuteData = function(origin, destination){
+		var service = new google.maps.DistanceMatrixService();
+		service.getDistanceMatrix(
+		{
+			origins: [origin],
+			destinations: [destination],
+			travelMode: 'TRANSIT'
+		}, litCommute.handleCommuteData);
+	}
+	
+
+	return(
+		<form>
+			<PlacesAutocomplete inputProps={inputPropsStart} />
+			<PlacesAutocomplete inputProps={inputPropsEnd} />
+			<button className="form-btn" type="submit">Calculate</button>
+		</form>
+	)
+}
+
+export default Form;
